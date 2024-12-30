cdfcad3d22732a1993f09278994f127dcffbc4ea
Kamil Wiśniowski <15131024-none-life@users.noreply.gitlab.com>
2024-12-30 02:21:06 +0100

    adding indent from store view structure

---
+++ view/adminhtml/web/js/model/stores.js
@@ -0,0 +1,12 @@
+/*eslint-disable */
+/* jscs:disable */
+define(["knockout"], function (_knockout) {
+  var active = _knockout.observable(null);
+  var counter = _knockout.observable({});
+  var _default = _exports = {
+    active: active,
+    counter: counter
+  };
+  return {};
+});
+//# sourceMappingURL=stores.js.map
